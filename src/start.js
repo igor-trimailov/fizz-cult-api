1d73a605d5b2b3c3f17b4926a4b5f859a88f6218
Igoris Trimailovas <igoris.trimailovas@sgdigital.com>
2020-06-27 12:26:08 +0100

    remove test env variables

--- src/start.js
+++ src/start.js
@@ -1,7 +1,6 @@
 const app = require('./server')
 
 // capture the port the app runs
-// TODO: does it make sense to have a fallback?
 const port = process.env.PORT || 5000
 app.listen(port, function() {
   console.log(`Node server listening on port ${port}`)
